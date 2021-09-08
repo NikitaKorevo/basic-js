59d713e5040fd79326f300b8ba8875234150d969
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-09-08 13:29:14 +0300

    feat: done extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,44 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+/*   
+  options.repeatTimes сколько повторяться будет это str
+  options.separator нужен между словами
+
+  options.addition присасывается к str
+  options.additionRepeatTimes сколько будет addition
+  options.additionSeparator соединяет addition между собой если их больше 1 */
+
+  let addition = [];
+  let additionSeparator = options.additionSeparator || '|';
+  let additionWithSeparator = '';
+
+  // сборка addition с additionSeparator
+  if (options.addition !== undefined) {
+    addition[0] = String(options.addition);
+
+
+    let additionRepeatTimes = options.additionRepeatTimes || 1;
+
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      addition[i] = addition[0];
+    }
+
+    additionWithSeparator = addition.join(additionSeparator);
+  }
+
+  let string = [];
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.separator || '+';
+  let stringWithAddition = '';
+
+  // сборка string c addition
+  for (let i = 0; i < repeatTimes; i++) {
+    string[i] = str + additionWithSeparator;
+  }
+
+  // сборка string + addition с separator
+  stringWithAddition = string.join(separator);
+  return stringWithAddition;
 }
