a5237f19554d2f0bd94cb39a48b9119655d97fea
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-09-09 20:29:01 +0300

    feat: done vigenere-cipher.js task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,65 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let encryptString = '';
+    let desiredLengthKey = '';
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    message = message.toUpperCase();
+
+    while (message.length > desiredLengthKey.length) {
+      desiredLengthKey += key;
+    }
+    desiredLengthKey = desiredLengthKey.slice(0, message.length).toUpperCase();
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i]) === -1) {
+        encryptString += message[i];
+      } else {
+        encryptString += alphabet[(alphabet.indexOf(message[i]) + alphabet.indexOf(desiredLengthKey[j])) % alphabet.length];
+        j++
+      }
+    }
+    
+    if (this.reverse) {
+      return encryptString;
+    } else {
+      return encryptString.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let decryptString = '';
+    let desiredLengthKey = '';
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    encryptedMessage = encryptedMessage.toUpperCase();
+
+    while (encryptedMessage.length > desiredLengthKey.length) {
+      desiredLengthKey += key;
+    }
+    desiredLengthKey = desiredLengthKey.slice(0, encryptedMessage.length).toUpperCase();
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (alphabet.indexOf(encryptedMessage[i]) === -1) {
+        decryptString += encryptedMessage[i];
+      } else {
+        decryptString += alphabet[(alphabet.indexOf(encryptedMessage[i]) - alphabet.indexOf(desiredLengthKey[j]) + alphabet.length) % alphabet.length];
+        j++
+      }
+    }
+
+    if (this.reverse) {
+      return decryptString;
+    } else {
+      return decryptString.split('').reverse().join('');
+    }
   }
 }
